4d81e65e85e19ddabe2cbf7f7a98ef7b80329438
abc <motocapstone2021@gmail.com>
2021-06-19 16:00:20 +0800

    Added note_item.xml Added NotesModel

--- app/src/main/java/com/silentdev/notesharing/models/NotesModel.java
+++ app/src/main/java/com/silentdev/notesharing/models/NotesModel.java
@@ -1,7 +1,40 @@
 package com.silentdev.notesharing.models;
 
+import com.google.firebase.Timestamp;
+
 public class NotesModel {
 
-    String title, body, userId;
+    String title, body, userId, sharerId;
+    Timestamp date_added, date_edited;
+    Boolean shared;
 
+    public NotesModel() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getSharerId() {
+        return sharerId;
+    }
+
+    public Timestamp getDate_added() {
+        return date_added;
+    }
+
+    public Timestamp getDate_edited() {
+        return date_edited;
+    }
+
+    public Boolean getShared() {
+        return shared;
+    }
 }
